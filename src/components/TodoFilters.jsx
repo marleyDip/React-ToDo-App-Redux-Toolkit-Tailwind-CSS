7ed330b4b158fa21f79ada7f07d5aea018662175
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-07-31 03:23:32 +0600

    to do filter

--- src/components/TodoFilters.jsx
+++ src/components/TodoFilters.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function TodoFilters() {
-  return <div>TodoFilters</div>;
+  return (
+    <div className="grid place-items-center">
+      <div className="inline-flex bg-gray-200">{/* logic */}</div>
+    </div>
+  );
 }
 
 export default TodoFilters;
